7ff96227fb85819a5b92b27e5677a6499e145fbd
SaiMadhavanG <g.saimadhavan@gmail.com>
2024-06-13 12:35:30 -0400

    changed config in extract_naps

--- extract_naps.py
+++ extract_naps.py
@@ -11,10 +11,10 @@ from torch.utils.data import DataLoader
 # Global Configs
 DELTA = 0.99
 NET_PATH = "./mnist_fc_64x4_adv_1.onnx"
-DATA = torch.load("./dataset/training.pt")
 EXPT_NAME = "mnist_fc_64x4_adv_1_d99"
 LAYERS = 4
 NEURONS_WIDTH = 64
+INPUT_SHAPE = (1, 1, 28, 28)
 
 # Data preprocessing
 
@@ -57,10 +57,11 @@ P = {
     {
         'net': NET_PATH,
         'delta': DELTA,
-        'data_len': DATA[1].shape[0],
+        'data_len': labels.shape[0],
         # FIXME: To make it compatible other architectures, specify o/p shapes of each node/layer
         'neurons_width': NEURONS_WIDTH,
-        'layers': LAYERS
+        'layers': LAYERS,
+        'input_shape': INPUT_SHAPE
     }
 }
 
